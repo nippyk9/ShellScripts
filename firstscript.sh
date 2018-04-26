bf6dbcc9ae3d4ff7757afb4b94855fc6474cdb5d
nippy <nippy9@gmail.com>
2018-04-26 08:31:12 +0530

    first shell

---
+++ firstscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Enter your name" 
+read name 
+
+echo "enter your age"
+read age
+
+echo "enter your city"
+read city
+
+echo "my name is $name"
+
+echo " I am $age old"
+
+
+echo "my city is $city"
